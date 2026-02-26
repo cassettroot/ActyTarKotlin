28ac185c402d672cdbf8ab345b49925a3eca88f1
cassettroot <martearteviejomar@gmail.com>
2026-02-25 23:26:25 -0600

    Actualizacion

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,22 @@
 fun main(){
     //Tarea 1
+    /*
     // act1(0,-10,5)
     // act2(150.0,false)
     // act3(105.0,"Programacion")
 
+     */
+
     //Tarea 2
+    /*
     // act4(3)
     // act5(5)
     // act6("azul")
     // act7("root")
     // act8(i = 1)
     // act9("Hola Kotlin",3)
+
+     */
 
     // act10
     /*
@@ -27,6 +33,65 @@ fun main(){
         println("El indice es $i")
     }
     */
+
+    //act12
+    /*
+    val calculadora = Calculadora()
+    println(calculadora.calcular(10.0,20.0,"sumar"))
+
+    val contar = Contador()
+    println(contar.incrementar())
+    println(contar.mostrar())
+    println(contar.incrementar())
+    println(contar.mostrar())
+
+     */
+
+    //Tar1 - U2
+    /*
+    var lampara = Lampara()
+    println(lampara.estado())
+    lampara.encender()
+    println(lampara.estado())
+
+     */
+
+    //Tar2 - U2
+    /*
+    var semaforo = Semaforo()
+    println(semaforo.estado())
+    semaforo.cambioVerde()
+    println(semaforo.estado())
+    */
+
+    //Tar3-U2
+    /*
+    var musica = Reproductor()
+    println(musica.reproducir("Circles"))
+    println(musica.estado())//Imprime mensaje
+    musica.pausar()
+    println(musica.estado())//Imprime mensaje
+    */
+
+    //Tar4-U2
+    /*
+    val temperatura = Termometro()
+    println(temperatura.mostrarTemp())
+    temperatura.subir(5.0)
+    println(temperatura.mostrarTemp())
+    temperatura.bajar(2.0)
+    println(temperatura.mostrarTemp())
+    */
+
+    //Tar5-U2
+
+    val door = Puerta()
+    println(door.estado())
+    door.abrir()
+    println(door.estado())
+    door.cerrar()
+    println(door.estado())
+
 
 
 }
